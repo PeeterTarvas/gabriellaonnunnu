751047cd1907c729ca011ba21bb18e89adb3e57a
peeter.tarvas <peetertarvas@gmail.com>
2025-02-13 21:43:50 +0100

    iniy

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import './src/style.css'
 import { setupCounter } from './src/counter.js'
 
+console.log("Main.js is running")
+
 document.querySelector('#app').innerHTML = `
     <div class="card">
         <h1 class="title">Kena sõbrapäeva nunnu,
@@ -9,16 +11,16 @@ document.querySelector('#app').innerHTML = `
          </h1>
         <div class="image-grid">
             <div class="animation">
-                <img src="images/cat-01.gif" class="cat img-fluid">
+                <img src="/images/cat-01.gif" class="cat img-fluid">
             </div>
             <div class="animation">
-                <img src="images/gabriellaonnuunnu1.jpg" class="cat img-fluid">
+                <img src="/images/gabriellaonnuunnu1.jpg" class="cat img-fluid">
             </div>
             <div class="animation">
-                <img src="images/gabriellaonnunnu2.jpg" class="cat img-fluid">
+                <img src="/images/gabriellaonnuunnu2.jpg" class="cat img-fluid">
             </div>
             <div class="animation">
-                <img src="images/gabriellaonnuunnu3.jpg" class="cat img-fluid">
+                <img src="/images/gabriellaonnuunnu3.jpg" class="cat img-fluid">
             </div>
         </div>
     </div>
